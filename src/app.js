a0549ea9f977b29261d1c9c2789aaa73514821d6
Atul Kumar Mishra <atulkm189@gmail.com>
2025-06-23 03:34:26 +0530

    team

--- src/app.js
+++ src/app.js
@@ -3,13 +3,24 @@ import cors from 'cors';
 import authRoutes from './routes/auth.js';
 import teamsRoutes from './routes/teams.js';
 import userRoutes from './routes/user.js';
+
 const app = express();
 
-app.use(cors());
+const allowedOrigins = [
+  'https://www.rebootbmsit.xyz',
+  'http://localhost:5713', 
+];
+
+app.use(cors({
+  origin: allowedOrigins,
+  credentials: true, // If you're using cookies or auth headers
+}));
+
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoutes);
 app.use('/api/faculty', teamsRoutes);
 app.use('/api/teams', teamsRoutes);
+
 export default app;
